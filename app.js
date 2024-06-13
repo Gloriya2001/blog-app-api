d8c4ab4db591af828d7fe7af9e7f6c16b9702dd3
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-13 11:24:40 +0530

    signup completed

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcryptjs")//for cyphertext
+
+const { blogmodel } = require("./models/blog")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://gloria2001:gloria2001@cluster0.ipg35w1.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Cluster0")
+
+
+const generateHashedPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)//salt is a cost
+    return bcrypt.hash(password, salt)
+}
+
+
+app.post("/signup", async (req, res) => {
+
+    let input = req.body
+    let hashedPassword = await generateHashedPassword(input.password)
+    console.log(hashedPassword)
+    input.password = hashedPassword
+    let blog = new blogmodel(input)
+    blog.save()
+
+    res.json({ "status": "success" })
+
+})
+
+
+
+
+app.listen(8080, () => {
+
+    console.log("Server running")
+})
